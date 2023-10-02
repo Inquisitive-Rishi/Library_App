ee4e20266f6721042e4c2b0cd0f5be25bc507e9c
rishi_raj <rishiraj007bgp@gmail.com>
2023-10-02 22:38:37 +0530

    display added books on card

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+const cardContainer = document.querySelector('.container');
 const addBtn = document.querySelector('.add-book');
 const submitBtn = document.querySelector('.submit-book');
-const showBtn = document.querySelector('.show-books')
+const showBtn = document.querySelector('.show-books');
 
 const myLibrary = [];
 
-function Book(name='none',author='none',pages=0) {
-    this.name = name
+function Book(title='none',author='none',pages=0) {
+    this.title = title
     this.author = author
     this.pages = pages
 }
@@ -27,9 +28,27 @@ submitBtn.addEventListener('click', ()=> {
     console.log(myLibrary);
 });
 
+
 showBtn.addEventListener('click', () => {
-    console.table(myLibrary);
+    for (let i = 0; i < myLibrary.length; i++) {
+        const card = document.createElement('div');
+        const h41 = document.createElement('h4');
+        const author = document.createElement('p');
+        const pages = document.createElement('p');
+    
+        card.classList.add('card');
+        h41.classList.add('title');
+        h41.textContent = `${myLibrary[i].title}`;
+        author.textContent = `Author: ${myLibrary[i].author}`;
+        pages.textContent = `Pages: ${myLibrary[i].pages}`;
+    
+        card.appendChild(h41)
+        card.appendChild(author)
+        card.appendChild(pages)
+        cardContainer.appendChild(card);
+    }
 });
+
 
 function addToLibrary() {
     myLibrary.push(book);
