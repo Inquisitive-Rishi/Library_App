50579e7f4e2c5def18194cf8dc0ef465a2950292
rishi_raj <rishiraj007bgp@gmail.com>
2023-10-02 20:09:07 +0530

    add books to array

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("HEllo World");
+const addBtn = document.querySelector('.add-book');
+const submitBtn = document.querySelector('.submit-book');
+
+const myLibrary = [];
+
+function Book(name='none',author='none',pages=0) {
+    this.name = name
+    this.author = author
+    this.pages = pages
+}
+
+let title;
+let author;
+let pages;
+let book;
+
+addBtn.addEventListener('click', () => {
+    title = prompt('Title?');
+    author = prompt("Author?") 
+    pages = parseInt(prompt('No of pages?'));
+    book = new Book(title,author,pages);
+})
+
+submitBtn.addEventListener('click', ()=> {
+    addToLibrary();
+    console.log(myLibrary);
+});
+
+function addToLibrary() {
+    myLibrary.push(book);
+}
