3b1f9d6b3956602bd0d2f0d3032fa7985a4a6b3d
rishi_raj <rishiraj007bgp@gmail.com>
2023-10-06 16:37:19 +0530

    card add functionality to array and nodelist

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+let title;
+let author;
+let pages;
+let book;
+
 const cardContainer = document.querySelector('.container');
 const showModalBtn = document.querySelector('.modal-btn');
+const closeModalBtn = document.querySelector('.close-btn');
 const dialog = document.querySelector('#dialog');
 const addCardBtn = document.querySelector('.add-card');
 
@@ -17,7 +23,48 @@ function Book(title,author,pages) {
 }
 
 showModalBtn.addEventListener('click', ()=> {
+    titleIpt.value = authorIpt.value = pagesIpt.value = '';
     dialog.showModal();
 })
+
+closeModalBtn.addEventListener('click', ()=> {
+    dialog.close();
+})
+
+addCardBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    title = titleIpt.value;
+    author = authorIpt.value;
+    pages = pagesIpt.value;
+    book = new Book(title,author,pages);
+    addToLibrary();
+    addToCard();
+    dialog.close();
+    console.log(myLibrary);
+});
+
+function addToLibrary() {
+    myLibrary.push(book);
+}
+
+function addToCard() {
+    const card = document.createElement('div');
+    const cardTitle = document.createElement('h3');
+    const cardAuthor = document.createElement('p');
+    const cardPages = document.createElement('p');
+    const removeBtn = document.createElement('button');
+
+    cardTitle.textContent = titleIpt.value;
+    cardPages.textContent = `${pagesIpt.value} pages`;
+    cardAuthor.textContent = `Written by: ${authorIpt.value}`;
+    removeBtn.textContent = 'remove';
+
+    card.setAttribute('class','card');
+    card.appendChild(cardTitle)
+    card.appendChild(cardAuthor)
+    card.appendChild(cardPages)
+    card.appendChild(removeBtn)
+    cardContainer.appendChild(card);
+}
 
 
