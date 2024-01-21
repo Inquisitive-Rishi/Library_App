de791e4d32d4305181f003ce58e8eaa8b4696441
Rishi Raj <rishiraj007bgp@gmail.com>
2024-01-21 20:44:42 +0530

    close modal button on click

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 const footer = document.querySelector('.footer');
 const addBtn = document.querySelector('header button')
 const modal = document.querySelector('dialog')
+const closeModalBtn = document.querySelector('.close-btn')
 
 addBtn.addEventListener('click', () => {
     modal.showModal()
+})
+
+closeModalBtn.addEventListener('click', () => {
+    modal.close()
 })
 
 footer.textContent = `Copyright © Rishi Raj ${new Date().getFullYear()}`
