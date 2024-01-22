0bb7fc7c6e38478418f65ddd13cb05c28badef90
Rishi Raj <rishiraj007bgp@gmail.com>
2024-01-22 20:57:09 +0530

    loop through library array to log book details

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const container = document.querySelector('.container')
 const footer = document.querySelector('footer');
 const addBtn = document.querySelector('header button')
 const modal = document.querySelector('dialog')
@@ -26,8 +26,8 @@ function Book(title,author,pages,checked) {
     this.checked = checked;
 }
 
-function pushBookToArray(book) {
-    library.push(book)
+function pushBookToArray(x) {
+    library.push(x)
 }
 
 addBookBtn.addEventListener('click', (e) => {
@@ -35,8 +35,26 @@ addBookBtn.addEventListener('click', (e) => {
     const book = new Book(inputTitle.value, inputName.value, inputPages.value, checkBox.checked)
     pushBookToArray(book)
     console.log(library);
+
+    const card = document.createElement('div')
+    const title = document.createElement('p')
+    const author = document.createElement('p')
+    const pages = document.createElement('p')
+    const isChecked = document.createElement('p')
+
+    card.setAttribute('class', 'card')
+
+    library.forEach(book => {
+        for (const [key, value] of Object.entries(book)) {
+            console.log(`${value}`);
+        }
+    })
+
+    container.appendChild(card)
     modal.close()
 })
+
+
 
 
 
